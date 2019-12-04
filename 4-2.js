41e842371987f976a4bd9ecbad28b6f4df4943e4
Joe Pikowski <joe.pikowski@gmail.com>
2019-12-04 08:07:09 -0500

    Add Day 4 Solutions

---
+++ 4-2.js
@@ -0,0 +1,22 @@
+/* Solution */
+function getValidPWsInRange(start, end){
+    let c = 0;
+    for (n = start; n <= end; n++){
+        if (isValid(n)){ c++; }
+    }
+    return c;
+}
+
+function isValid(n){
+    let adjacent = false;
+    const a = (n+'').split('').map(m=>parseInt(m));
+    if (a.length != 6){ return false; }
+    for (const [i,int] of a.entries()){
+        if (a[i] < a[i-1]){ return false; }
+        if (!adjacent && a[i] === a[i-1] && a[i] !== a[i+1] && a[i] !== a[i-2]){ adjacent = true; }
+    }
+    return adjacent;
+}
+
+/* Input */
+console.log( getValidPWsInRange(284639,748759) );
